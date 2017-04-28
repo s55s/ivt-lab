52a38d4888edf88b9ca54050a72e9a2c5bf15819
Zsolt <zsolth5@gmail.com>
2017-04-28 00:02:35 -0700

    +fireTorpedos switch default case

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -86,6 +86,8 @@ public class GT4500 implements SpaceShip {
 		firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
 	}
         break;
+      default:
+        break;
     }
 
     return firingSuccess;
